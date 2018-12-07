0ebabdca49258ed506c00a8f3c3c26f8c83f2c12
Mradul Pandey <pandeymradul@gmail.com>
2018-12-08 01:46:56 +0530

    Added demo pipline

---
+++ vars/demoPipline.groovy
@@ -0,0 +1,25 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, url: pipelineParams.gitUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+        }
+        post {
+            failure {
+                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
+            }
+        }
+    }
+
+}
