34200603755fb29f0af0978995c8060a9d6287e6
Mradul Pandey <pandeymradul@gmail.com>
2018-12-08 23:25:49 +0530

    Adding new step

--- vars/demoPipline.groovy
+++ vars/demoPipline.groovy
@@ -2,27 +2,41 @@ def call(Map pipelineParams) {
 
     pipeline {
         agent any
-        tools{
+
+        tools {
             maven 'maven3'
             jdk 'jdk8'
         }
+
         stages {
-            stage('checkout git') {
+
+            stage('Checkout Git') {
                 steps {
                     git branch: pipelineParams.branch, url: pipelineParams.gitUrl
                 }
             }
 
-            stage('build') {
+
+            stage('Determine version') {
+                steps {
+                    currentBuild.displayName = "# Some version"
+                }
+            }
+
+            stage('Build') {
                 steps {
                     sh 'mvn clean package -DskipTests=true'
                 }
             }
+
         }
+
         post {
+
             failure {
                 mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
             }
+
         }
     }
 
