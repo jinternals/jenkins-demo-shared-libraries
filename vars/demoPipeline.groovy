3a293d3bbcddc7c2eee0734447c444cf7fd4700e
Mradul Pandey <pandeymradul@gmail.com>
2018-12-10 23:32:50 +0530

    Updated file name

--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -1,11 +1,4 @@
-def call(body) {
-
-    // evaluate the body block, and collect configuration into the object
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
-
+def call(Map pipelineParams) {
 
     pipeline {
         agent any
