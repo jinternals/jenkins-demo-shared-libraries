a8a88e8545f6086e15a68f91bbaee789c34adc39
Mradul <pandeymradul@gmail.com>
2019-07-09 14:20:28 +0530

    Update domainPipeline.groovy

--- vars/domainPipeline.groovy
+++ vars/domainPipeline.groovy
@@ -3,6 +3,7 @@ def call(Map pipelineParams) {
     def label = "jenkins-slave-${UUID.randomUUID().toString()}"
 
     podTemplate(label: label, containers: [
+            containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:latest', args: '${computer.jnlpmac} ${computer.name}'),
             containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')
     ]) {
 
