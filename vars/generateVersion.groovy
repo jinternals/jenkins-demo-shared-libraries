78f5b3492cf63df6d9ea636525dae4dbabe0122d
Mradul <pandeymradul@gmail.com>
2019-07-09 18:45:31 +0530

    Update generateVersion.groovy

--- vars/generateVersion.groovy
+++ vars/generateVersion.groovy
@@ -8,20 +8,14 @@ def call(Map opts = [:]) {
 
      try {
        // if the repo has no tags this command will fail
-       sh 'git describe --tags $(git rev-list --tags --max-count=1) > version.tmp'
-       def previousVersion = readFile 'version.tmp'
-       echo 'found previous tagged version ' + previousVersion
-
-       try {
-         // if there's an int as the version then turn it into a major.minor.micro version
-         def myInt = '1' previousVersion int
-         return myInt + '.0.1'
-       } catch (err) {
-         // otherwise lets use the traditional version style
+         sh 'git describe --tags $(git rev-list --tags --max-count=1) > version.tmp'
+         def previousVersion = readFile 'version.tmp'
+         echo 'found previous tagged version ' + previousVersion
+         
          def microVersion = previousVersion.substring(previousVersion.lastIndexOf('.')+1) as int
          version = previousVersion.substring(0, previousVersion.lastIndexOf('.')+1) + (microVersion+1)
-       }
-       echo 'using new version ' +  version
+      
+         echo 'using new version ' +  version
      } catch (err) {
        echo "no existing tag found or issue understanding existing tags so using ${version}"
      }
