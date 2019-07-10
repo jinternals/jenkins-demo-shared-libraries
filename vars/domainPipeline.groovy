8cf1cf893aa1b9ddc8a1ddee7a6cdb3db622ec63
Mradul <pandeymradul@gmail.com>
2019-07-10 14:27:44 +0530

    Update domainPipeline.groovy

--- vars/domainPipeline.groovy
+++ vars/domainPipeline.groovy
@@ -2,43 +2,52 @@ def call(Map pipelineParams) {
 
     def label = "jenkins-slave-${UUID.randomUUID().toString()}"
 
-    podTemplate(label: label, containers: [
-            containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:latest', args: '${computer.jnlpmac} ${computer.name}'),
-            containerTemplate(name: 'maven', image: 'maven:3.5-jdk-8', ttyEnabled: true, command: 'cat')], 
-    volumes:[
-        hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
-        hostPathVolume(hostPath: '/root/.m2', mountPath: '/root/.m2')
-    ]) {
+    podTemplate(
+            label: label,
+            containers: [
+                    containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:latest', args: '${computer.jnlpmac} ${computer.name}'),
+                    containerTemplate(name: 'docker', image: 'docker:18.02', ttyEnabled: true, command: 'cat'),
+                    containerTemplate(name: 'maven', image: 'maven:3.5-jdk-8', ttyEnabled: true, command: 'cat')
+            ],
+            volumes: [
+                    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                    hostPathVolume(hostPath: '/root/.m2', mountPath: '/root/.m2')
+            ]) {
 
         node(label) {
-             stage('Checkout') {
+            
+            stage('Checkout') {
                 git credentialsId: 'github', url: "${pipelineParams.gitUrl}"
-             } 
+            }
+            
             stage('Determine Version') {
                 versionNumber = generateVersion(pom: 'pom.xml')
-                 
+
                 echo "Generated new tag ${versionNumber}"
-                
-                 try {
-                      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
+
+                try {
+                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
                         sh "git config credential.username ${env.GIT_USERNAME}"
                         sh "git config credential.helper '!f() { echo password=\$GIT_PASSWORD; }; f'"
                         sh "git tag ${versionNumber}"
                         sh "GIT_ASKPASS=true git push origin ${versionNumber}"
-                      }
-                 } 
-                 catch (Exception err) {
+                    }
+                }
+                catch (Exception err) {
                     currentBuild.result = 'FAILURE'
-                 }
-                 finally {
+                }
+                finally {
                     sh "git config --unset credential.username"
                     sh "git config --unset credential.helper"
-                 }
-                 
+                }
+
                 container('maven') {
-                    stage('Build a Maven project') {
-                        sh 'mvn -B clean install'
+                    
+                    stage('Build') {
+                        sh "mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${versionNumber}"
+                        sh "mvn -B clean install"
                     }
+                    
                 }
             }
         }
