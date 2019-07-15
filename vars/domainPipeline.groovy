a600941e2ad8ae6080ce79664ba894bcc7c8685e
Mradul <pandeymradul@gmail.com>
2019-07-15 22:20:05 +0530

    Update domainPipeline.groovy

--- vars/domainPipeline.groovy
+++ vars/domainPipeline.groovy
@@ -65,6 +65,13 @@ def call(Map pipelineParams) {
                     }
                 }
             }
+            
+            stage ('Trigger CI') {
+                
+                    build job: 'ci-pipeline', parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: "${versionNumber}"]]
+
+            
+            }
         }
     }
 
