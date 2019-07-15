36370ebd428f7989d6de39172f145466cca27cb7
Mradul <pandeymradul@gmail.com>
2019-07-15 18:49:52 +0530

    Create ciPipeline.groovy

---
+++ vars/ciPipeline.groovy
@@ -0,0 +1,25 @@
+def call(Map pipelineParams) {
+
+    def label = "jenkins-ci-slave-${UUID.randomUUID().toString()}"
+
+    podTemplate(
+            label: label,
+            cloud: 'jenkins',
+            containers: [
+                    containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:latest', args: '${computer.jnlpmac} ${computer.name}'),
+                    containerTemplate(name: 'docker', image: 'docker:18.02', ttyEnabled: true, command: 'cat')
+            ],
+            volumes: [
+                    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+            ]) {
+
+        node(label) {
+
+            stage('Deploy') {
+                echo
+            }
+
+        }
+    }
+
+}
