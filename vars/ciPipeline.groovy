56f3aa15a1ecbe2bfe8bc692a359c17762767293
Mradul <pandeymradul@gmail.com>
2019-07-29 23:06:54 +0530

    Update ciPipeline.groovy

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -8,7 +8,7 @@ def call(Map pipelineParams) {
             containers: [
                     containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:latest', args: '${computer.jnlpmac} ${computer.name}'),
                     containerTemplate(name: 'docker', image: 'docker:18.02', ttyEnabled: true, command: 'cat'),
-                    containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.8', command: 'cat', ttyEnabled: true)
+                    containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.15.1', command: 'cat', ttyEnabled: true)
             ],
             volumes: [
                     hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
