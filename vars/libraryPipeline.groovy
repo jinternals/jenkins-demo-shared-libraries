e6386228bde9559d2f9d71f68f0662c86218eb83
Mradul <pandeymradul@gmail.com>
2019-07-30 21:29:42 +0530

    Update libraryPipeline.groovy

--- vars/libraryPipeline.groovy
+++ vars/libraryPipeline.groovy
@@ -7,33 +7,32 @@ def call(Map pipelineParams) {
             cloud: 'jenkins',
             containers: [
                     containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:latest', args: '${computer.jnlpmac} ${computer.name}'),
-                    containerTemplate(name: 'docker', image: 'docker:18.02', ttyEnabled: true, command: 'cat'),
-                    containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.15.1', command: 'cat', ttyEnabled: true)
+                    containerTemplate(name: 'maven', image: 'maven:3.5-jdk-8', ttyEnabled: true, command: 'cat')
             ],
             volumes: [
-                    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                    hostPathVolume(hostPath: '/root/.m2', mountPath: '/root/.m2')
             ]) {
 
         node(label) {
-            
-           stage('Checkout') {
+
+            stage('Checkout') {
                 try {
-                    git branch:  "${pipelineParams.gitBranch}", credentialsId: "${pipelineParams.gitCredentialId}", url: "${pipelineParams.gitRepository}"
-                } catch (e) {
-                    throw e;
-                }
-           }
-    
-           stage('Determine Version') {
-                try {
-                    versionNumber = generateVersion(pom: 'pom.xml')
-                    currentBuild.displayName = "# ${versionNumber}"
-                    createGitTag(pipelineParams,versionNumber)
+                    git branch: "${pipelineParams.gitBranch}", credentialsId: "${pipelineParams.gitCredentialId}", url: "${pipelineParams.gitRepository}"
                 } catch (e) {
                     throw e;
                 }
             }
-            
+
+            stage('Determine Version') {
+                try {
+                    versionNumber = generateVersion(pom: 'pom.xml')
+                    currentBuild.displayName = "# ${versionNumber}"
+                    createGitTag(pipelineParams, versionNumber)
+                } catch (e) {
+                    throw e;
+                }
+            }
+
             stage('Build Artifacts') {
                 container('maven') {
                     sh "mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${versionNumber}"
@@ -43,7 +42,7 @@ def call(Map pipelineParams) {
 
 
             stage('Publish Artifacts') {
-                       sh "Publish Artifacts"
+                sh "Publish Artifacts"
             }
         }
     }
