46e08307adb9590c88dbe7e4c3bf9f475dca136a
Mradul <pandeymradul@gmail.com>
2019-07-30 19:43:39 +0530

    Update substituteTemplate.groovy

--- vars/substituteTemplate.groovy
+++ vars/substituteTemplate.groovy
@@ -1,4 +1,9 @@
 def call(String inputFile , Map options = [:], String outputFile) {
+  transform(inputFile, options, outputFile)
+}
+
+@NonCPS
+def transform(String inputFile , Map options = [:], String outputFile) {
   def templateContent = readFile inputFile
   def engine = new groovy.text.SimpleTemplateEngine()
   def result = engine.createTemplate(templateContent).make(options)
