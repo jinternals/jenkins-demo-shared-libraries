06e4118858f0a9710e2c831c30ea1516396ffebb
Mradul <pandeymradul@gmail.com>
2019-07-30 18:43:11 +0530

    Update and rename template.groovy to substituteTemplate.groovy

--- vars/substituteTemplate.groovy
+++ vars/substituteTemplate.groovy
@@ -3,5 +3,5 @@ def call(String fileName,Map options = [:]) {
   def templateContent = readFile fileName
   def engine = new groovy.text.SimpleTemplateEngine()
   def result = engine.createTemplate(templateContent).make(options)
-  println result.toString()
+  return result.toString()
 }
