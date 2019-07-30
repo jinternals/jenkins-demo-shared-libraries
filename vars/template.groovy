50ec986191051d38a3a4ffd1a10eba0a4a4b3e79
Mradul <pandeymradul@gmail.com>
2019-07-30 15:25:01 +0530

    Update template.groovy

--- vars/template.groovy
+++ vars/template.groovy
@@ -1,7 +1,7 @@
 
 def call(String fileName,Map options = [:]) {
-  der template = readFile fileName
+  def templateContent = readFile fileName
   def engine = new groovy.text.SimpleTemplateEngine()
-  def result = engine.createTemplate(template).make(options)
+  def result = engine.createTemplate(templateContent).make(options)
   println result.toString()
 }
