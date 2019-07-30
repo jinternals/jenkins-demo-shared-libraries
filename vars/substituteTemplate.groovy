6665c9471e35f160c8fb8a0b4b994f7daa9c7c68
Mradul <pandeymradul@gmail.com>
2019-07-30 21:06:13 +0530

    Update substituteTemplate.groovy

--- vars/substituteTemplate.groovy
+++ vars/substituteTemplate.groovy
@@ -1,12 +1,11 @@
 def call(String inputFile , Map options = [:], String outputFile) {
-  transform(inputFile, options, outputFile)
+  _transform(inputFile, options, outputFile)
 }
 
-def transform(String inputFile , Map binding = [:], String outputFile) {
+def _transform(String inputFile , Map binding = [:], String outputFile) {
   def input = readFile inputFile
   def engine = new org.apache.commons.lang3.text.StrSubstitutor(binding)
   def templateOutput = engine.replace(input)
   engine = null
   writeFile file: outputFile, text: templateOutput
-  sh "ls"
 }
