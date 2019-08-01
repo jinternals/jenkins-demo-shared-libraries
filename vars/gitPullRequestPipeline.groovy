e2a3d93938f93f1b9560dbe850b5c642ac1677ee
Mradul <pandeymradul@gmail.com>
2019-08-01 22:46:22 +0530

    Update gitPullRequestPipeline.groovy

--- vars/gitPullRequestPipeline.groovy
+++ vars/gitPullRequestPipeline.groovy
@@ -44,8 +44,8 @@ def call(Map pipelineParams) {
                           withSonarQubeEnv('sonar') { 
                               sh "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar " + 
                               "-f pom.xml " +
-                              "-Dsonar.organization=${pipelineParams.sonar_organization} " +
-                              "-Dsonar.projectKey=${pipelineParams.sonar_projectKey} " +
+                              "-Dsonar.organization=${pipelineParams.sonarOrganization} " +
+                              "-Dsonar.projectKey=${pipelineParams.sonarProjectKey} " +
                               "-Dsonar.login=$TOKEN"
                          }
                       }
