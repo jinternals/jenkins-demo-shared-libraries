7a8c5c730105e20e3b456df93ac3927998250755
Mradul Pandey <pandeymradul@gmail.com>
2019-08-03 14:45:57 +0530

    Removed un-used files

--- vars/toAlphanumeric.groovy
+++
@@ -1,7 +0,0 @@
-/**
- * toAlphanumeric converts text to strictly alphanumeric form.
- * Example: a_B-c.1 -> abc1
- */
-def call(Map opts = [:]) {
-    opts.text.toLowerCase().replaceAll("[^a-z0-9]", "")
-}
